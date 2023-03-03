c5be8055ebf6cb8da10d65c42f41c4ca202c309d
vilicvane <vilicvane@live.com>
2023-03-03 23:07:10 +0800

    WIP

--- src/cli/commands/deploy.ts
+++ src/cli/commands/deploy.ts
@@ -1,8 +1,14 @@
 import {Command} from '@oclif/core';
 
+import {pack} from '../core';
+
 export class Deploy extends Command {
   async run(): Promise<void> {
-    console.log('deploy');
+    const code = await pack(process.cwd());
+
+    const codeBuffer = Buffer.from(code);
+
+    console.log(codeBuffer.length);
   }
 
   static override description = '将脚本打包部署到盯梢提供的托管环境。';
