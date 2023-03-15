9e624b28f33c151a4086890ee6be502c63d0f0dc
vilicvane <vilicvane@live.com>
2023-03-16 06:13:41 +0800

    Add --last flag

--- src/cli/commands/logs.ts
+++ src/cli/commands/logs.ts
@@ -1,16 +1,31 @@
+import {Flags} from '@oclif/core';
+import ms from 'ms';
+
 import {Command} from '../@command';
 import {ensureAccessToken, pollLogs, printLogEvent} from '../@core';
 
-export class Log extends Command {
+export class Logs extends Command {
   async run(): Promise<void> {
     const {entrances} = this;
 
+    const {
+      flags: {last},
+    } = await this.parse(Logs);
+
+    const startTime = last ? new Date(Date.now() - ms(last)) : undefined;
+
     await ensureAccessToken(entrances);
 
-    for await (const event of pollLogs(entrances)) {
+    for await (const event of pollLogs(entrances, {startTime})) {
       printLogEvent(event);
     }
   }
 
   static override description = '查看脚本日志。';
+
+  static override flags = {
+    last: Flags.string({
+      description: '最近一段时间，例如 "1d"',
+    }),
+  };
 }
